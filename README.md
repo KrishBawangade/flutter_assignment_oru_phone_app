09f7336f6e30982bcdaf775e4861335981b942e9
KrishBawangade <krishbawangade08@gmail.com>
2025-02-09 05:08:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_assignment_oru_phone_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_assignment_oru_phone_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_assignment_oru_phone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
